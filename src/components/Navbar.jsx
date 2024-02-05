384f6acb26d6d0bf2068393fea31ffed73ae2c54
Drina <driinamusaj@gmail.com>
2024-02-05 14:26:39 +0100

    navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,47 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import {HashLink as Link} from 'react-router-hash-link';
 import logo from '../assets/images/logo.png'
 import linkedin from '../assets/images/linkedin.png'
 import behance from '../assets/images/behance.png'
 import mail from '../assets/images/mail.png'
 
 
+
 const Navbar = () =>{
-    const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }
-
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
-
+ 
     return(
         <nav class="navbar navbar-expand-lg navbar-dark">
   <div class="container-fluid">
+
     <a class="navbar-brand d-flex" href="#">
         <img src={logo} alt="logo" width={130}/>
     </a>
-    <button class="navbar-toggler toggler-style" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
       <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <li>
+        <Link to='#home' className="link">
+            Home
+          </Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="#">About</a>
+        <li>
+          <Link to='#about' className="link">
+            About
+          </Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="#">Skills</a>
+        <li>
+        <Link to='#projects' className="link">
+            Projects
+          </Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="#">Projects</a>
+        <li>
+        <Link to='#contact' className="link">
+            Contact
+          </Link>
         </li>
       </ul>
       <div className="social-icon">
